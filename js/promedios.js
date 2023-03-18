fd90d601fe05453a4f9f121e49c114b8030e7ab4
Sala D15 <paortegon@uniboyaca.edu.com>
2023-03-18 09:58:30 -0500

    Se modificó index.html para poder añadir promedios.js

---
+++ js/promedios.js
@@ -0,0 +1,33 @@
+let actividades = [
+    {id:1, nombre:"Actividad 1", nota:0},
+    {id:2, nombre:"Actividad 2", nota:1.5},
+    {id:3, nombre:"Actividad 3", nota:5}
+]
+
+function mostrarMensaje(promedio){
+    let mensaje = promedio>=3?"Felicitaciones aprobó":"Lo sentimos vuelva a intentar"
+    document.getElementById("mensajeNota").innerText = mensaje
+}
+
+function mostrarActividades(){
+    let filas = ""
+    let sumatoria = 0
+    for(let actividad of actividades){
+        filas += "<tr>"
+        filas += "  <td>"+actividad.id+"</td>"
+        filas += "  <td>"+actividad.nombre+"</td>"
+        filas += "  <td>"+actividad.nota+"</td>"
+        filas += "  <td>"
+        filas += "      <button>Modficar</button>"
+        filas += "      <button>Eliminar</button>"
+        filas += "  </td>"
+        filas += "</tr>"
+        sumatoria += actividad.nota
+    }
+    let promedio = sumatoria / actividades.length
+    document.getElementById("promediosTb").getElementsByTagName("tbody")[0].innerHTML = filas
+    document.getElementById("promedioText").innerText = "Su promedio es: "+ promedio
+    mostrarMensaje(promedio)
+}
+
+mostrarActividades()
