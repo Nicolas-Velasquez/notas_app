91e7b1c7ea4f0242a94c9ff1c700708992480e1e
Sala D13 <CSalaD13@uniboyaca.edu.co>
2023-03-23 20:46:37 -0500

    ahora funcionan los botones de crear, modificar y eliminar

--- js/promedios.js
+++ js/promedios.js
@@ -3,6 +3,7 @@ let actividades = [
     {id:2, nombre:"Actividad 2", nota:1.5},
     {id:3, nombre:"Actividad 3", nota:5}
 ]
+let indexActividadSeleccionada = -1
 
 function mostrarMensaje(promedio){
     let mensaje = promedio>=3?"Felicitaciones aprobó":"Lo sentimos vuelva a intentar"
@@ -12,17 +13,18 @@ function mostrarMensaje(promedio){
 function mostrarActividades(){
     let filas = ""
     let sumatoria = 0
-    for(let actividad of actividades){
+    for(let index in actividades){
+        let actividad = actividades[index]
         filas += "<tr>"
         filas += "  <td>"+actividad.id+"</td>"
         filas += "  <td>"+actividad.nombre+"</td>"
         filas += "  <td>"+actividad.nota+"</td>"
         filas += "  <td>"
-        filas += "      <button>Modficar</button>"
-        filas += "      <button>Eliminar</button>"
+        filas += "      <button onclick='modificarActividad("+index+")'>Modficar</button>"
+        filas += "      <button onclick='eliminarActividad("+index+")'>Eliminar</button>"
         filas += "  </td>"
         filas += "</tr>"
-        sumatoria += actividad.nota
+        sumatoria += parseFloat(actividad.nota)
     }
     let promedio = sumatoria / actividades.length
     document.getElementById("promediosTb").getElementsByTagName("tbody")[0].innerHTML = filas
@@ -31,3 +33,53 @@ function mostrarActividades(){
 }
 
 mostrarActividades()
+
+document.getElementById("crearBtn").addEventListener("click", () =>{
+    indexActividadSeleccionada = -1
+    document.getElementById("actividad").setAttribute("value", "")
+    document.getElementById("nota").setAttribute("value", "")
+    document.getElementById("formActividad").reset()
+    document.getElementById("formularioModal").classList.remove("close-modal")
+    document.getElementById("tituloModal").innerText = "Registrar actividad"
+})
+
+document.getElementById("cerrarModal").addEventListener("click", () =>{
+    document.getElementById("formularioModal").classList.add("close-modal")
+})
+
+document.getElementById("aceptarModal").addEventListener("click", () =>{
+    let formulario = document.forms["formularioActividad"]
+    let actividad = formulario["actividad"].value
+    let nota = formulario["nota"].value
+
+    if(indexActividadSeleccionada==-1){
+        //crear
+        let id = actividades.length + 1
+        actividades.push({
+            id: id,
+            nombre: actividad,
+            nota: nota
+        })
+    }else{
+        actividades[indexActividadSeleccionada].nombre = actividad
+        actividades[indexActividadSeleccionada].nota = nota 
+    }
+    mostrarActividades()
+    
+    document.getElementById("formularioModal").classList.add("close-modal")
+})
+
+function modificarActividad(posicionArray){
+    indexActividadSeleccionada = posicionArray
+    document.getElementById("formActividad").reset()
+    document.getElementById("formularioModal").classList.remove("close-modal")
+    document.getElementById("tituloModal").innerText = "Modificar actividad"
+    let actividad = actividades[posicionArray]
+    document.getElementById("actividad").setAttribute("value", actividad.nombre)
+    document.getElementById("nota").setAttribute("value", actividad.nota)
+}
+
+function eliminarActividad(index){
+    actividades.splice(index, 1)
+    mostrarActividades()
+}
